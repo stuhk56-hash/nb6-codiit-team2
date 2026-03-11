9b0b095ae4b948413d49ee84084384faf5fceeeb
hyeonjun <stuhk56@gmail.com>
2026-03-11 09:07:09 +0900

    feat/users

--- src/modules/users/structs/users.struct.ts
+++ src/modules/users/structs/users.struct.ts
@@ -1,16 +1,19 @@
+import { UserType } from '@prisma/client';
 import * as s from 'superstruct';
 import { emailString } from '../../../lib/validation/struct-helpers';
 
+const PasswordStruct = s.size(s.string(), 8, 20);
+
 export const CreateUserBodyStruct = s.type({
-  type: s.optional(s.enums(['SELLER', 'BUYER'])),
+  type: s.optional(s.enums(Object.values(UserType))),
   name: s.size(s.string(), 2, 10),
   email: emailString,
-  password: s.size(s.string(), 8, 20),
+  password: PasswordStruct,
 });
 
 export const UpdateMeBodyStruct = s.type({
-  currentPassword: s.size(s.string(), 8, 20),
+  currentPassword: PasswordStruct,
   name: s.optional(s.size(s.string(), 2, 10)),
   email: s.optional(emailString),
-  password: s.optional(s.size(s.string(), 8, 20)),
+  password: s.optional(PasswordStruct),
 });
